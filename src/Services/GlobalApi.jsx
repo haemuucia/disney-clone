b3eae0db006bd7b8e23bc5464921ef5af1d1c3ad
Veronica Irene Tanumihadja <162682171+haemuucia@users.noreply.github.com>
2025-08-04 11:19:39 +0700

    apus api key

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const movieBaseUrl="https://api.themoviedb.org/3"
-const api_key = '96cc9e9b1594c4c1f8b9089cf1b2eb54'
-//https://api.themoviedb.org/3/list/1?api_key=96cc9e9b1594c4c1f8b9089cf1b2eb54&language=en-US
-const movieByGenreBaseURL = 'https://api.themoviedb.org/3/discover/movie?api_key=96cc9e9b1594c4c1f8b9089cf1b2eb54&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres='
+const api_key = 'YOUR_API_KEY' //change to your API key from TMDB
+
+const movieByGenreBaseURL = 'https://api.themoviedb.org/3/discover/movie?api_key=YOUR_API_KEY&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres='
 
 const getTrendingVideos = () => {
     axios.get(movieBaseUrl + "/trending/all/day?api_key=" + api_key);
